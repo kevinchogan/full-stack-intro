6d674676270388d3ddf4e6b5ff2d529630ed540e
Kevin Hogan <kchogan@pacbell.net>
2024-05-02 14:43:13 -0700

    added post and user models as well as read, add, and delete routes

---
+++ components/Posts.jsx
@@ -0,0 +1,12 @@
+import DeletePostButton from "./DeletePostButton";
+
+export default function Post({id, title, content, authorName}){
+    return (
+        <div style={{border: '1px solid black', padding: '15px', margin: '10px 0'}}>
+            <h3>{authorName}</h3>
+            <h4>{title}</h4>
+            <p>{content}</p>
+            <DeletePostButton postId={id}/>
+        </div>
+    )
+}
